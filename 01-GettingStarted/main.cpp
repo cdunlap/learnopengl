32eb45f16aa81bd3d989d4979a18ca36d2a01e94
Cale Dunlap <cale.dunlap@gmail.com>
2017-03-05 04:46:48 -0800

    Key callback and clear color

--- 01-GettingStarted/main.cpp
+++ 01-GettingStarted/main.cpp
@@ -2,6 +2,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
 int main()
 {
 	glfwInit();
@@ -18,6 +24,7 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
@@ -33,6 +40,11 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+		
+		// Render commands
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 	}
 
